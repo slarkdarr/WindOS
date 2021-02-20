f08958691fa4eb97b68c920b602e00f18c876ef3
darubagus <13519080@std.stei.itb.ac.id>
2021-02-20 22:38:51 +0700

    a n j a y

--- kernel.c
+++ kernel.c
@@ -1,78 +1,103 @@
 void handleInterrupt21 (int AX, int BX, int CX, int DX);
+void clearscreen();
+void printLogo(char* c,int baris, int kolom, int warna);
 void printString(char *string);
 void readString(char *string);
 void clear(char *buffer, int length);
 
+
 int main () {
-  putInMemory(0xB000, 0x8000, 'H');
-  putInMemory(0xB000, 0x8001, 0xD);
-  putInMemory(0xB000, 0x8002, 'a');
-  putInMemory(0xB000, 0x8003, 0xD);
-  putInMemory(0xB000, 0x8004, 'i');
-  putInMemory(0xB000, 0x8005, 0xD);
+	char* string;
+
+	clearscreen();
+	printLogo("Hai",0,0,0xD);
+
+	
+	readString(string);
 
   while (1);
 }
 
-void clear(char* buffer, int length){
-	int i;
-	for(i=0;i<length;i++){
-		buffer[i] = 0x0;
+void clearscreen(){
+  /* Membersihkan tulisan x86 blablabla dari layar */
+  int i=0;
+  for(i=0; i<25; i++)
+    printLogo("                                                                                          ",i,0,0x02);
+}
+
+
+void printLogo(char* c,int baris, int kolom, int warna){
+	/* Menulis string pada baris dan kolom dengan warna sesuai parameter fungsi */
+	int i, b, alamat;
+	i = 0;
+
+	alamat = (baris)*80*2 + 2*kolom;
+	while(c[i] != '\0'){
+		b = alamat;
+		putInMemory(0xB000, b+0x8000, c[i]);
+		putInMemory(0xB000, b+0x8001, warna);
+		i = i + 1;
+		alamat = alamat + 2;
+	}
+
+}
+
+void printString(char* string){
+	int i =0;
+	while (string[i] != '\0'){
+		int ch = string[i];
+		interrupt(0x10, 0xE*256+ch, 0, 0, 0);
+		i++;
 	}
 }
 
-void printString(char* chars){
-
-   
+void readString(char* string){
+    int dashn = 0xa;	// \n character new line biasa
+    int endStr = 0x0; 	// blank
+    int dashr = 0xd;	// 
+    char backspace[4];	// set of command buat backspace
+    char CC = '\0';
     int i = 0;
-    while(chars[i] != '\0'){
-            int ch = chars[i];
-            /* interrupt #, AX, BX, CX, DX */
-		        interrupt(0x10, 0xe*256+ch, 0, 0, 0);
-            i++;
+
+    char enter[3];		// set of commands buat enter
+    enter[0] = '\r';
+    enter[1] = '\n';
+    enter[2] = '\0';
+
+    
+    backspace[0] = '\b';
+    backspace[1] =  ' ';
+    backspace[2] = '\b';
+    backspace[3] = '\0';
+    
+    
+
+    while (CC != '\r'){
+    	CC = interrupt(0x16, 0x0, 0x0, 0x0, 0x0);
+
+    	switch(CC){
+    		case '\0':
+    			break;
+    		case '\b' :
+    			if (i>0){
+    				i--;
+    				string[i]= '\0';
+    				printString(backspace);
+    			}
+    			break;
+    		case '\r':
+    			break;
+    		default:
+    			string[i] = CC;
+    			i++;
+    			interrupt(0x10, 0xE*256+CC, 0, 0, 0);
+    				
+    	}
     }
-
-
-}
-
-void readString(char* buff){
-    int dashn = 0xa;
-    int endStr = 0x0;
-    int enter = 0xd;
-    int backsp = 0x8;
-    int dashr = 0xd;
-    int loop = 1;
-    int count = 2;
-    buff[0] = dashr;
-    buff[1] = dashn;
-    while(loop){
-            /* Call interrupt 0x16 */
-            /* interrupt #, AX, BX, CX, DX */
-            int ascii = interrupt(0x16,0,0,0,0);
-            if (ascii == enter){              
-				buff[count] = 0x0;
-                buff[count+1] = dashr;
-                buff[count+2] = dashn;
-                return;
-            }
-            else if (ascii == backsp){
-                if (count > 1){
-                    buff[count] = 0x0;
-                    count--;
-                    interrupt(0x10,0xe*256+0x8,0,0,0);
-                    count++;
-                    interrupt(0x10,0xe*256+0x0,0,0,0);
-                    count--;
-                    interrupt(0x10,0xe*256+0x8,0,0,0);
-                    
-               }
-            }
-            else{
-                buff[count] = ascii;
-                interrupt(0x10, 0xe*256+ascii, 0, 0, 0);
-                count++;
-            }     
-    }
+    string[i] = '\0';
+    printString(enter);
+    printString(string);
+    
 }
 
 void handleInterrupt21 (int AX, int BX, int CX, int DX){	
