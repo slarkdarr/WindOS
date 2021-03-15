99c57336451004c48bd27b8f3ce834b1207744a9
salsabiilashifa11 <salsabiilashifa11@gmail.com>
2021-03-16 00:04:06 +0700

    delete

--- NewOS/library.h
+++ NewOS/library.h
@@ -11,7 +11,7 @@ void moveFile(char *path, char *targetFolder, int *success, char parentIndex);
 void copyFile(char *path, char *targetFolder, int *success, char parentIndex);
 void writeFile(char *buffer, char *path, int *success, char parentIndex);
 void readFile(char *buffer, char *path, int *success, char parentIndex);
-void deleteFileIdx(char Idx,char* dir,int*success);
+// void deleteFileIdx(char Idx,char* dir,int*success);
 
 // void makeDir(char *foldername, char *success, char parentIndex);
 void delete(char* filename, int* success, char parentIndex);
