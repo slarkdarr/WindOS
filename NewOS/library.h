1e14d8e0f850f1e6273c3cd78c91459b4fc13863
salsabiilashifa11 <salsabiilashifa11@gmail.com>
2021-03-15 23:55:01 +0700

    update library.h

--- NewOS/library.h
+++ NewOS/library.h
@@ -13,12 +13,12 @@ void writeFile(char *buffer, char *path, int *success, char parentIndex);
 void readFile(char *buffer, char *path, int *success, char parentIndex);
 void deleteFileIdx(char Idx,char* dir,int*success);
 
-void makeDir(char *foldername, char *success, char parentIndex);
-void delete(char* filename, int* success, char parentIndex);
-void deleteIdx(char Idx, char* dir,int* success);
-void deleteFolder(char *path, char *resultCode, char parentIndex);
-void deleteFolderIdx(char Idx);
-void listContent(char Idx,char* dir);
+// void makeDir(char *foldername, char *success, char parentIndex);
+// void delete(char* filename, int* success, char parentIndex);
+// void deleteIdx(char Idx, char* dir,int* success);
+// void deleteFolder(char *path, char *resultCode, char parentIndex);
+// void deleteFolderIdx(char Idx);
+// void listContent(char Idx,char* dir);
 char getDirIndex(char *path, char* dir, char parentIndex);
 char getPathIndex(char*path, char* dir, char parentIndex);
 
